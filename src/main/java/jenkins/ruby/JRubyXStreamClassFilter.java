ea493fec6cc63215860d29392e234274f5bea37b
Jesse Glick <jglick@cloudbees.com>
2018-01-12 11:48:24 -0500

    Found (experimentally) some more things that would need to be whitelisted.

--- src/main/java/jenkins/ruby/JRubyXStreamClassFilter.java
+++ src/main/java/jenkins/ruby/JRubyXStreamClassFilter.java
@@ -2,6 +2,7 @@ package jenkins.ruby;
 
 import hudson.Extension;
 import jenkins.security.CustomClassFilter;
+import org.jenkinsci.jruby.JRubyMapper;
 import org.jenkinsci.jruby.JRubyXStream;
 import org.jenkinsci.jruby.JavaProxyConverter;
 import org.jruby.RubyArray;
@@ -14,6 +15,7 @@ import org.jruby.RubyString;
 import org.jruby.RubySymbol;
 import org.jruby.java.proxies.ConcreteJavaProxy;
 import org.jruby.javasupport.proxy.InternalJavaProxy;
+import org.jruby.runtime.builtin.IRubyObject;
 
 /**
  * See JEP-200.
@@ -34,6 +36,8 @@ public class JRubyXStreamClassFilter implements CustomClassFilter {
             c == RubyObject.class ||
             c == RubyString.class ||
             c == RubySymbol.class ||
+            c == IRubyObject.class ||
+            c == JRubyMapper.DynamicProxy.class ||
             c == ConcreteJavaProxy.class ?
                 true : null;
     }
